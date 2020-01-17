e0506dbae94e89867c3e9ec8758a72632b696bd7
coultat <felipemkautzmann@gmail.com>
2020-01-17 15:49:31 +0100

    this is the very beginning

---
+++ api.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+def create_app(db_name):
+    app = Flask(__name__)
+
+    @app.route('/status', methods=['GET'])
+    def status():
+        return jsonify({'message': 'ok'})
+
+    @app.route('/resp', methods=['POST'])
+    def resp():
+        print(request.json)
+        return jsonify(request.json)
+
+
+    @app.route('/bands', methods=['POST', 'GET'])
+    def bands():
+        if request.method == 'POST':
+            args = request.json
+            conn = sqlite3.connect(db_name)
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO bands(name, members, birth, genre)VALUES(?,?,?,?)'''
+                           ,(args['name'], args['members'], args['birth'], args['genre'])
+            )
+            conn.commit()
+            conn.close()
+            return jsonify({'message':'if you are reading this is because something went very well :-)'})
+        if request.method == 'GET':
+            return jsonify({'message':'if you are reading this is because was asked in the DB'})
+
+
+    @app.route('/bands/<name>', methods=['GET', 'DELETE', 'PUT'])
+    def list_bands(name):
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        if request.method == 'GET':
+            cursor.execute('''SELECT * FROM bands WHERE id = ?''', (name,))
+            row = cursor.fetchall()
+            print(row)
+            return jsonify({'message': 'é nois'})
+        if request.method == 'DELETE':
+            cursor.execute(''' DELETE FROM bands WHERE id = ?''', (name,))
+        if request.method == 'PUT':
+            cursor.execute('''UPDATE bands SET name = (?), members = (?), birth = (?),genre = (?) 
+                        WHERE id = '?';''', (args['name'], args['members'], args['birth'], args['genre'], name))
+
+
+
+    return app
+
+app = create_app
+
+
+if __name__ == '__main__':
+    my_app = create_app('rascunhos.db')
+    my_app.run()
---
+++ test_api.py
@@ -0,0 +1,116 @@
+import pytest
+import sqlite3
+from rascunhos import create_app
+
+#é como se a funçao estivesse na primeira linha
+@pytest.fixture
+def rascunhos_client():
+    app = create_app('rascunhos.db')
+    client = app.test_client()
+    yield client
+
+
+@pytest.fixture
+def rascunhos_db():
+    conn = sqlite3.connect('rascunhos.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS bands(id integer primary key autoincrement not null, name text, members integer, birth date, genre text);''')
+    yield c
+    #c.execute('''DROP TABLE bands''')
+
+
+def test_bands_post(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.post(
+    '/bands',
+    json={
+        "name": "the rolling stones",
+        "members": 4,
+        "birth": "1962",
+        "genre": "rock"
+    })
+    assert response.status_code == 200
+
+
+def test_bands_get(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.get(
+    '/bands')
+    assert response.status_code == 200
+    connection = sqlite3.connect('rascunhos.db')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT * FROM BANDS""")
+    connection.commit()
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+def test_list_bands(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.get('/bands/2')
+    assert response.status_code == 200
+    assert response.json['message'] == 'é nois'
+
+
+
+def test_status(rascunhos_client):
+    response = rascunhos_client.get('/status')
+    assert response.status_code == 200
+    assert response.json['message'] == 'ok'
+
+
+def test_respuesta(rascunhos_client):
+    texto = 'qué elegancia la de Francia'
+    response = rascunhos_client.post('/resp', json={'mensaje': texto})
+    assert response.status_code == 200
+    assert response.json['mensaje'] == texto
+
+'''
+def test_bands(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.post(
+        '/bands',
+        json={
+            "name":"turma da mônica",
+            "members": 4,
+            "birth": "1959",
+            "genre": "tirinhas"
+        })
+    assert response.status_code == 200
+    connection = sqlite3.connect('rascunhos.db')
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT * FROM bands""")
+    print(cursor.fetchall())
+    assert response.json['message'] == 'if you are reading this is because something went very well :-)'
+'''
+
+def test_can_save_band_to_db(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.post(
+        '/bands',
+        json={
+            "name": "soulfly",
+            "members": 4,
+            "birth": '1997',
+            "genre": "death_metal"
+        })
+    assert response.status_code == 200
+    connection = sqlite3.connect('rascunhos.db')
+    cursor = connection.cursor()
+    cursor.execute(''' SELECT *
+                    FROM bands
+                    WHERE name = "soulfly"''')
+    row = cursor.fetchall()
+
+
+def test_testando_coisas_com_o_put(rascunhos_client, rascunhos_db):
+    response = rascunhos_client.put(
+        '/putz',
+        json={
+            "name": "Capital Inicial",
+            "members": 4,
+            "birth": "1987",
+            "genre": "mpb"
+        })
+    #assert response.status_code == 200
+    #connection = sqlite3.connect('rascunhos.db')
+    #cursor = connection.cursor()
+    #cursor.execute('''UPDATE bands SET name = "Capital Inicial", members = 4, birth = "1982", genre = "mpb"
+     #               WHERE name = "soulfly" ''')
+    #assert response.json['message'] == 'es Belo, es Forte, Impávido Colosso'
